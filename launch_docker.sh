713c8d9a3f79c46471259f1605fcf879e6752caf
james <jameslathamsmith@gmail.com>
2020-01-30 09:32:17 -0800

    include realsense

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,1 @@
-rocker --oyr-run-arg " --rm --privileged --device=/dev/video0:/dev/video0" --nvidia --x11 --user --home --net host --pulse melodic-ur-robotiq:latest
+rocker --oyr-run-arg " --rm --privileged --device=/dev/video0:/dev/video0" --x11 --user --home --net host --pulse melodic-ur-robotiq:latest
